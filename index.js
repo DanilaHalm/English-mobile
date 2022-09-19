23dc28cd61f24689e5e9a4b7f791c02aec9e98da
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2022-09-19 14:52:55 +0300

    test listener

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const MENU_CONTAINER = document.querySelector(".menu_container"); 
 const ABOUT_PAGE = document.querySelector(".about");
-    
+const GAMES_PAGE = document.querySelector(".game");
+const CONTACT_PAGE = document.querySelector(".contact");
+
 function showPage(e) {
   const EVENT_TARGET = e.target; 
+  alert(EVENT_TARGET);
     if (EVENT_TARGET.dataset.item === "about") {
       ABOUT_PAGE.style.top = "0";
     }
