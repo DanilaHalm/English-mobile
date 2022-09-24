67745a7649aa07f94da2251173af9cb553667af4
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2022-09-24 22:49:24 +0300

    Update index.js
    Updt

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+const MENU = document.querySelector(".menu");
 const MENU_CONTAINER = document.querySelector(".menu_container"); 
 const ABOUT_PAGE = document.querySelector(".about");
 const GAMES_PAGE = document.querySelector(".game");
 const CONTACT_PAGE = document.querySelector(".contact");
 
+function animateMenu(isMenuVisible) { 
+  if(isMenuVisible) { 
+    MENU.classList.remove("menu_show");
+    MENU.classList.add("menu_hide");
+    MENU.style.height = "0";
+  } else { 
+    MENU.classList.remove("menu_hide");
+    MENU.classList.add("menu_show");
+    MENU.style.height = "40px";
+  }
+}
+
 function showPage(event) {
   const EVENT_TARGET = event.target;
     if (EVENT_TARGET.dataset.item === "about") {
+      animateMenu(true);
       ABOUT_PAGE.classList.add("about_anim");
       ABOUT_PAGE.style.top = "0";
     }
